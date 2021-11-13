2a0fdec4fe3480dcd26f8decbd5670b149e63e42
Kristoffer Kabat <yeah-kris@hotmail.com>
2021-11-13 23:26:35 +0100

    SRSLevels: Add documentation and a few minor changes.
    Inline the querySelectorAll call that gets the list items.
    Add individual stages for Locked and Lesson items. Map them to a new
    Other category for combined stages.

--- src/srs_levels.ts
+++ src/srs_levels.ts
@@ -33,7 +33,7 @@ const SRS_LEVELS_STYLE = `
 
 interface Item {
     assignments: {
-        srs_stage: number,
+        srs_stage: SRSIndividualStage,
     }
     data: {
         level: number,
@@ -59,33 +59,51 @@ interface SRSLevelsSettings {
     let settings: WKOFDialog;
     let data: { level: number, total_items: number, per_stage: number[] }[];
 
-    let stage_dom_items = Array.from(document.querySelectorAll(".srs-progress li"));
+    // Create a template element that will be the base for the output of this script.
+    // This is done because it is faster to clone than it is to create a new element each time.
+    // NOTE: This small performance gain is not important in this case, but the small complexity increase
+    // is not enough of a drawback in this case to not use it.
+    const template = document.createElement("div");
+    template.classList.add(SCRIPT_ID, "levels_cleared");
+
     let stage_divs: HTMLDivElement[] = [];
-    const el = document.createElement("div");
-    el.classList.add(SCRIPT_ID, "levels_cleared");
-    for(let item of stage_dom_items) {
+    // Append a clone of the template element to each element in 'stage_dom_items'
+    for(let item of document.querySelectorAll(".srs-progress li")) {
+        // Increase the bottom padding, to make room for the element
         const padding_bottom = Number(window.getComputedStyle(item).paddingBottom.slice(0, -2));
         (item as HTMLLIElement).style.setProperty("padding-bottom", `${padding_bottom + 28}px`);
 
-        const node = el.cloneNode() as HTMLDivElement;
+        // Clone the template and append it to the list element
+        const node = template.cloneNode() as HTMLDivElement;
         item.append(node);
+        // Push the element to a list to be able to use it later
         stage_divs.push(node);
     }
 
     setup();
+    /** 
+     * Load the necessary plugins from wkof, create the settings menu and add a link to the menu.
+     * Fetch all assignments from wanikani from the levels up to and including the current and
+     * only save the total number of items per level and the stage of each them.
+     * Each item is also counted towards all previous stages they've cleared.
+     * 
+     * NOTE: Should only be called once, on script start-up.
+    */
     async function setup() {
         wkof.include("Menu,Settings,ItemData")
         await wkof.ready("Menu,Settings,ItemData");
 
+        // Setup the settings menu
         settings = prepare_srs_levels_settings_dialog(update);
         await settings.load({ threshold: 90 });
         wkof.Menu.insert_script_link({
             name: `${SCRIPT_ID}_settings`,
             submenu: "Settings",
             title: "Levels by SRS",
-            on_click: open_settings,
+            on_click: settings.open,
         });
 
+        // Get the assignments starting from level one and up to the current level.
         const items: Item[] = await wkof.ItemData.get_items({
             wk_items: {
                 options: { assignments: true },
@@ -94,54 +112,74 @@ interface SRSLevelsSettings {
         });
 
         const item_by_level: { level: number, total_items: number, per_stage: number[] }[] = [];
+        // Sort the items into the level they are from and save only the total number of items
+        // as well as the amount per stage
         for(let i of items) {
             let level = i.data.level;
             let stage = srs_map(i.assignments.srs_stage);
+            // If the entry for the current level is undefined, create a new one and fill the 'per_stage' arary with 0's.
             if(item_by_level[level] === undefined) {
-                item_by_level[level] = { level: level, total_items: 0, per_stage: Array(stage_dom_items.length) };
+                item_by_level[level] = { level: level, total_items: 0, per_stage: Array(stage_divs.length) };
                 item_by_level[level].per_stage.fill(0);
             }
             item_by_level[level].total_items++;
+            if(stage === SRSCombinedStage.OTHER) {
+                continue;
+            }
+            // Increment the value for all stages less than or equal to the current.
+            // Ex: if the stage is 'Master' also increment 'Guru' and 'Apprentice'.
             for(let idx = stage; idx >= 0; idx--) {
                 item_by_level[level].per_stage[idx]++;
             }
         }
+        // Reverse the array, so that the last level is first (This step allows to skip a lot of computation later).
         data = item_by_level.reverse();
+        // Remove the empty 0th element that is now at the end. This prevents a undefined check later.
         data.pop();
 
         update();
     }
 
+    /**
+     * Calculate the highest level cleared for each stage and display it in the output divs ('stage_divs' array).
+     * 
+     * NOTE: Should be called once, on script start-up, and every time the settings are saved.
+     */
     async function update() {
         const settings = wkof.settings[SCRIPT_ID] as SRSLevelsSettings;
 
         let threshold = settings.threshold;
-        const max_level_on_stage = Array(stage_dom_items.length).fill(0);
+        const max_level_on_stage = Array(stage_divs.length).fill(0);
+        // For every item, check if they (given the threshold) have cleared the stage.
         for(let level of data) {
+            // Count how many stages where skipped (already set by a later level).
             let continued = 0;
             for(let i = 0; i < max_level_on_stage.length; i++) {
+                // Skip this stage if the current level is lower than the one already set
                 if(max_level_on_stage[i] > level.level) {
                     continued++;
                     continue;
                 }
+                // Check if the level has cleared the stage.
                 if(level.per_stage[i] / level.total_items * 100 >= threshold) {
                     max_level_on_stage[i] = level.level;
                 }
             }
-            if(continued >= 5) {
+            // If all of the stages are already set to a level higher than this,
+            // break out of the loop, as the order of the levels are descending,
+            // none of the remaining levels will be needed to be checked.
+            // Ex: Current level = 5. max_level_on_stage = [10, 9, 9, 7, 6].
+            if(continued >= max_level_on_stage.length) {
                 break;
             }
         }
 
+        // Output the highest level cleared for each stage
         max_level_on_stage.forEach((val, i) => {
             stage_divs[i].innerText = `Level: ${val}`;
-        })
+        });
     }
 
-
-    function open_settings() {
-        settings.open();
-    }
 
     const style = document.createElement("style");
     style.id = `${SCRIPT_ID}_style`;
@@ -150,12 +188,14 @@ interface SRSLevelsSettings {
     document.head.append(style);
 })();
 
-function dbg<T>(obj: T, ...other: any[]): T {
-    console.debug(...other, obj);
-    return obj;
-}
-
-function prepare_srs_levels_settings_dialog(update: Function) {
+/**
+ * Create the settings dialog.
+ * 
+ * NOTE: This function should only be called once and the result be saved.
+ * @param update The function to call when the "Save"-button is pressed
+ * @returns The created WKOFDialog
+ */
+function prepare_srs_levels_settings_dialog(update: Function): WKOFDialog {
     return new window.wkof.Settings({
         script_id: SCRIPT_ID,
         title: "Levels by srs",
@@ -171,9 +211,8 @@ function prepare_srs_levels_settings_dialog(update: Function) {
     });
 }
 
-
 enum SRSIndividualStage {
-    APPRENTICE_1 = 1, APPRENTICE_2 = 2, APPRENTICE_3 = 3, APPRENTICE_4 = 4,
+    LOCKED = -1, LESSON = 0, APPRENTICE_1 = 1, APPRENTICE_2 = 2, APPRENTICE_3 = 3, APPRENTICE_4 = 4,
     GURU_1 = 5, GURU_2 = 6, MASTER = 7,
     ENLIGHTENED = 8, BURN = 9,
 }
@@ -184,8 +223,16 @@ enum SRSCombinedStage {
     MASTER,
     ENLIGHTENED,
     BURN,
+    OTHER,
 }
 
+/**
+ * Maps an individual stage to a combined stage.
+ * Ex: Apprentice 2 -> Apprentice
+ * 
+ * @param srs The individual stage to map
+ * @returns The combined stage that the 'srs' individual stage belongs to
+ */
 function srs_map(srs: SRSIndividualStage): SRSCombinedStage {
     switch(srs) {
         case SRSIndividualStage.APPRENTICE_1:
@@ -206,5 +253,9 @@ function srs_map(srs: SRSIndividualStage): SRSCombinedStage {
 
         case SRSIndividualStage.BURN:
             return SRSCombinedStage.BURN;
+
+        case SRSIndividualStage.LOCKED:
+        case SRSIndividualStage.LESSON:
+            return SRSCombinedStage.OTHER;
     }
 }
